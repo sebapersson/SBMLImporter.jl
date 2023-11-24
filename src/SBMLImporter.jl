ebef912d052ae39fe612b73710c35d644d656d0e
sebapersson <sebastian.persson1@gmail.com>
2023-11-24 07:12:41 +0100

    Add PrecompileTools

--- src/SBMLImporter.jl
+++ src/SBMLImporter.jl
@@ -3,6 +3,7 @@ module SBMLImporter
 using DiffEqCallbacks
 using ModelingToolkit
 using OrdinaryDiffEq
+using PrecompileTools
 using RuntimeGeneratedFunctions
 using SBML
 using SpecialFunctions
@@ -24,6 +25,20 @@ include("Events.jl")
 include("SBML_math.jl")
 include("Initial_assignments.jl")
 include("Reactions.jl")
+
+
+# Model with piecewise for PrecompileTools to make model reading faster
+@setup_workload begin
+    path_SBML = joinpath(@__DIR__, "..", "test", "Models", "model_Brannmark_JBC2010.xml")
+    ode_system, specie_map, parameter_map, cb, get_tstops, ifelse_t0 = SBML_to_ODESystem(path_SBML, ret_all=true)
+    ode_problem = ODEProblem(ode_system, specie_map, (0.0, 10.0), parameter_map, jac=true)
+    for _f! in ifelse_t0
+        _f!(ode_problem.u0, ode_problem.p)
+    end
+    tstops = get_tstops(ode_problem.u0, ode_problem.p)
+    sol = solve(ode_problem, Rodas5P(), abstol=1e-8, reltol=1e-8, tstops=tstops, callback=cb)
+end
+
 
 export SBML_to_ODESystem
 
