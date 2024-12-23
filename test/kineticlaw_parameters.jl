55fb8bc2c8cb413b7168d2f9785bc43ab1386fb5
sebapersson <sebastian.persson1@gmail.com>
2024-12-23 08:50:34 +0100

    Update kinetic law test

--- test/kineticlaw_parameters.jl
+++ test/kineticlaw_parameters.jl
@@ -8,11 +8,11 @@ using SBMLImporter, Catalyst, OrdinaryDiffEq, Test
 # First case with no duplicate ids
 path_SBML = joinpath(@__DIR__, "Models", "brusselator_kinetic_parameters.xml")
 prn1, cb1 = load_SBML(path_SBML; inline_kineticlaw_parameters = false)
-@test string.(parameters(prn1.rn)) == ["B", "A", "k1_r2", "k1_r4", "C"]
+@test string.(parameters(prn1.rn)) == ["B", "A", "k1_r2", "k1_r4", "compartment"]
 @test string(prn1.p[3]) == "k1_r2 => 1.0"
 @test string(prn1.p[4]) == "k1_r4 => 1.0"
 prn2, cb2 = load_SBML(path_SBML; inline_kineticlaw_parameters = true)
-@test string.(parameters(prn2.rn)) == ["B", "A", "C"]
+@test string.(parameters(prn2.rn)) == ["B", "A", "compartment"]
 oprob1 = ODEProblem(prn1.rn, prn1.u0, (0.0, 10.0), prn1.p)
 sol1 = solve(oprob1, Rodas5P())
 oprob2 = ODEProblem(prn2.rn, prn2.u0, (0.0, 10.0), prn2.p)
