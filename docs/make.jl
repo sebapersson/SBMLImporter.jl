1d565de27a8406475e5ecef9bd11ef40a803cb2e
sebapersson <sebastian.persson1@gmail.com>
2026-01-11 08:34:10 +0000

    Format docs make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,25 +3,31 @@ using Documenter
 
 DocMeta.setdocmeta!(SBMLImporter, :DocTestSetup, :(using SBMLImporter); recursive = true)
 
-format = Documenter.HTML(;prettyurls = get(ENV, "CI", "false") == "true",
-                         assets = String["assets/custom_theme.css"],
-                         repolink = "https://github.com/sebapersson/SBMLImporter.jl",
-                         edit_link = "main")
+format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    assets = String["assets/custom_theme.css"],
+    repolink = "https://github.com/sebapersson/SBMLImporter.jl",
+    edit_link = "main"
+)
 
-makedocs(; modules = [SBMLImporter],
-         repo = "https://github.com/sebapersson/SBMLImporter.jl/blob/{commit}{path}#{line}",
-         checkdocs = :exports,
-         warnonly = false,
-         format = format,
-         sitename = "SBMLImporter.jl",
-         pages = [
-             "Home" => "index.md",
-             "Tutorial" => "tutorial.md",
-             "API" => "API.md",
-             "Supported SBML features" => "support.md",
-             "Other SBML related Julia packages" => "differences.md",
-             "FAQs" => "FAQ.md",
-         ],)
+makedocs(;
+    modules = [SBMLImporter],
+    repo = "https://github.com/sebapersson/SBMLImporter.jl/blob/{commit}{path}#{line}",
+    checkdocs = :exports,
+    warnonly = false,
+    format = format,
+    sitename = "SBMLImporter.jl",
+    pages = [
+        "Home" => "index.md",
+        "Tutorial" => "tutorial.md",
+        "API" => "API.md",
+        "Supported SBML features" => "support.md",
+        "Other SBML related Julia packages" => "differences.md",
+        "FAQs" => "FAQ.md",
+    ],
+)
 
-deploydocs(; repo = "github.com/sebapersson/SBMLImporter.jl.git",
-           devbranch = "main",)
+deploydocs(;
+    repo = "github.com/sebapersson/SBMLImporter.jl.git",
+    devbranch = "main",
+)
