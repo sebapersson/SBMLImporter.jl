47ad7fd6e295755c2388e133a8aef3ff997f7de8
sebapersson <sebastian.persson1@gmail.com>
2026-03-07 07:53:44 +0000

    Use FBDF for hard-to-simulate tests

--- test/semantic_sbml_tests.jl
+++ test/semantic_sbml_tests.jl
@@ -193,7 +193,7 @@ solver = Rodas4P()
         end
 
         # Simulations fail with Rodas4P, so FBDF is used
-        if test_case in ["00028", "00173", "00269", "01260", "01504", "01669"]
+        if test_case in ["00028", "00173", "00269", "01260", "01504", "01669", "01670", "01671"]
             check_test_case(test_case, FBDF())
             continue
         end
